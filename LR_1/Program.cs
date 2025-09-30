a5a5974b9436aec53e8a4227e626421e974718f3
User <User@CR11>
2025-09-30 12:22:37 +0300

    Добавьте файлы проекта.

---
+++ LR_1/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LR_1
+{
+    class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+
+    class UserRegistration
+    {
+        private List<User> users = new List<User>();
+        public bool RegisterUser(string email, string password)
+        {
+            string RestorationCode = CodeMailSender(email);
+
+            Console.Write("Enter the 6-digit code that was sent to your email: ");
+            string CompareCodes = Console.ReadLine();
+            if (CompareCodes != RestorationCode)
+            {
+                Console.WriteLine("Неверный код!");
+                return false;
+            }
+            // Проверка корректности электронной почты и пароля
+            if (IsValidEmail(email) && IsStrongPassword(password))
+            {
+                users.Add(new User { Email = email, Password = password });
+                Console.WriteLine("Пользователь успешно зарегистрирован");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка регистрации пользователя");
+                return false;
+            }
+        }
+        private bool IsValidEmail(string email)
+        {
+            // Простейшая проверка формата email для примера
+            try
+            {
+                MailAddress m = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsStrongPassword(string password)
+        {
+            // Проверка надежности пароля (просто для примера - обычно используются более сложные методы)
+            return password.Length >= 8;
+        }
+
+        private bool IsUser(string email)
+        {
+            foreach (var user in users)
+            {
+                if (user.Email == email)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool LoginUser(string email, string password)
+        {
+            if (!IsUser(email))
+            {
+                Console.WriteLine("Неверный адрес электронной почты или пароль");
+                return false;
+            }
+            else
+            {
+                foreach (var user in users)
+                {
+                    if (user.Email == email && user.Password == password)
+                    {
+                        Console.WriteLine("Вход выполнен успешно");
+                        return true;
+                    }
+                }
+                Console.WriteLine("Неверный адрес электронной почты или пароль");
+                return false;
+            }
+                
+        }
+        public void Info(string email)
+        {
+            foreach (var user in users)
+            {
+                if (user.Email == email)
+                {
+                    Console.WriteLine("Вход пароль:", user.Password);
+
+                }
+            }
+            Console.WriteLine("Такого пользователя нет");
+
+        }
+
+        private string CodeMailSender(string UserEmail)
+        {
+            Random RandGen = new Random();
+            string RestorationCode = RandGen.Next(99999, 1000000).ToString();
+
+            MailAddress from = new MailAddress("lr1mailmessages@gmail.com", "Maxim");
+            MailAddress to = new MailAddress(UserEmail);
+            MailMessage RestorationMessage = new MailMessage(from, to);
+            RestorationMessage.Subject = "Your password restoration letter.";
+            RestorationMessage.Body = $"<h1>Hello! Your code is </h1>\n<h2> {RestorationCode} </h2>";
+            RestorationMessage.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("lr1mailmessages@gmail.com", "llfp xaex ylgk ccgq");
+            smtp.EnableSsl = true;
+            smtp.Send(RestorationMessage);
+            return RestorationCode;
+        }
+
+        public void PasswordRestoration()
+        {
+            Console.Write("Enter the email the account was registered to: ");
+            string UserEmail = Console.ReadLine();
+            if (!IsValidEmail(UserEmail) || !IsUser(UserEmail))
+            {
+                Console.Write("The email is invalid: ");
+            }
+
+            string RestorationCode = CodeMailSender(UserEmail);
+
+            Console.Write("Enter the 6-digit code that was sent to your email: ");
+            string CompareCodes = Console.ReadLine();
+            if (CompareCodes == RestorationCode)
+            {
+                Console.Write("Enter your new password: ");
+                string NewPassword = Console.ReadLine();
+                foreach (var user in users)
+                {
+                    if (user.Email == UserEmail)
+                    {
+                        user.Password = NewPassword;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            UserRegistration userRegistration = new UserRegistration();
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:\n1 - Регистрация\n2 - Вход\n3 - Восстановить \n0 - Выход");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Введите адрес электронной почты:");
+                        string regEmail = Console.ReadLine();
+
+                        Console.WriteLine("Введите пароль:");
+                        string regPassword = Console.ReadLine();
+
+                        userRegistration.RegisterUser(regEmail, regPassword);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Введите адрес электронной почты:");
+                        string loginEmail = Console.ReadLine();
+
+                        Console.WriteLine("Введите пароль:");
+                        string loginPassword = Console.ReadLine();
+
+                        userRegistration.LoginUser(loginEmail, loginPassword);
+                        break;
+
+                    case "3":
+                        userRegistration.PasswordRestoration();
+                        break;
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+                        break;
+                }
+            }
+        }
+    }
+
+}
